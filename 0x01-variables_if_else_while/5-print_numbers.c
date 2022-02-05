a103febe7930e54ca2eb9764d3a844bc72fd9c66
Your Name <you@example.com>
2022-02-04 23:10:07 -0800

    created a sequency of numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the string in the function
+ * Description: using the main function for
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: Allways 0 (Success)
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	printf("%d", i);
+	}
+	putchar(10);
+	return (0);
+}
