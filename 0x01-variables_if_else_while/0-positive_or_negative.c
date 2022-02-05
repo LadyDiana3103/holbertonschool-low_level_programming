a00e774dcfcd6fba0304320b969a3e3f379bf6c5
Your Name <you@example.com>
2022-02-04 17:07:45 -0800

    Using printf

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the string in the function
+ * Description: using the main function IF
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: Allways return 0
+ */
+
+int main(void)
+{
+	int n;
+
+	Srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive,\n", n);
+
+	if (n == 0)
+		printf("%d is zero,\n", n);
+
+	if (n < 0)
+		printf("%d is negative,\n", n);
+
+	return (0);
+}
