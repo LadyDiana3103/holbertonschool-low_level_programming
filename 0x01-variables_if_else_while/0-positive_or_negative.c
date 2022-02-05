1f953e6feaafe8693f3430c68648eb6edfce3cfc
Your Name <you@example.com>
2022-02-04 18:38:47 -0800

    Updating status

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,7 @@
  * Description: using the main function IF
  * this program prints "Programming is like building a multilingual puzzle"
  * Return: Allways return 0
- **/
+ */
 
 int main(void)
 {
@@ -17,13 +17,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d is positive,\n", n);
+		printf("%d is positive\n", n);
 
 	if (n == 0)
-		printf("%d is zero,\n", n);
+		printf("%d is zero\n", n);
 
 	if (n < 0)
-		printf("%d is negative,\n", n);
+		printf("%d is negative\n", n);
 
 	return (0);
 }
