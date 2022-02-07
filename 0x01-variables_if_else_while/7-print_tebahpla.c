613083beb7d84107dc0ed88f21a2dc03a09ced1d
Your Name <you@example.com>
2022-02-06 19:34:21 -0800

    Exchanging results

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prviints the string in the function
+ * Description: using the main function for
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: Allways 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 122;
+
+	for (i = 122; i >= 97; i--)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
