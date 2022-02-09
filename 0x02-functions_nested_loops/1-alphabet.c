98b88fc32a6529cd35828eaeb2a469b2b7b46008
Your Name <you@example.com>
2022-02-08 20:35:30 -0800

    Alphabeth sequencial

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,19 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Diana Quispe
+ */
 #include "main.h"
 /**
- * main - prints the main function
- * Description: this program prints the alphabet
- * this program prints "Programming is like building a multilingual puzzle"
- * Return: Allways 0 (Success)
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * Return: allways 0 SUCCES.
  */
-void print_alphabet(void)
+
+int print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	_putchar(letter);
 	_putchar('\n');
-
+	return (0);
 }
