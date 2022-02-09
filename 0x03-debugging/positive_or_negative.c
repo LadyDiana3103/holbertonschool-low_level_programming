2ef1ffce3758e8083d4cd8bd2532ee3b8c541abf
Your Name <you@example.com>
2022-02-09 13:58:42 -0800

    File debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void positive_or_negative(int n)
+{
+	if (n > 0)
+	printf("%d is positive\n", n);
+
+	if (n == 0)
+	printf("%d is zero\n", n);
+
+	if (n < 0)
+	printf("%d is negative\n", n);
+}
