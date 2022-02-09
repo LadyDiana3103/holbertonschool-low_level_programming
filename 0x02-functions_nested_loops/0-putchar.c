8387ab34d81523144140491e25a1a35a790bd708
Your Name <you@example.com>
2022-02-08 18:30:05 -0800

    ataching header

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - prints the main function
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return (0);
+ */
+int main(void)
+{
+	char a = 95;
+	char b = 112;
+	char c = 117;
+	char d = 116;
+	char e = 99;
+	char f = 104;
+	char g = 97;
+	char h = 114;
+
+	_putchar(a);
+	_putchar(b);
+	_putchar(c);
+	_putchar(d);
+	_putchar(e);
+	_putchar(f);
+	_putchar(g);
+	_putchar(h);
+	_putchar(10);
+
+	return (0);
+}
+
