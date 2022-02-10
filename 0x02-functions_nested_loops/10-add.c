6e37770fbc30c6e8230983c43bdbdfffbe52cb9a
Your Name <you@example.com>
2022-02-09 21:08:09 -0800

    sum two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * add- add two integers
- * @a: two integer arguments
- * @b: two integer arguments
- * return: sum
+ * add -add two integers.
+ * @a: two integer arguments.
+ * @b: two integer arguments.
+ * Return: sum
  */
 
 int add(int a, int b)
 {
-	return (a +b);
+	return (a + b);
 }
